736e8376077e577a01d4835ef8fabbd7ab34b9e9
Daniel Matcau <daniel.matcau@gmail.com>
2014-08-23 11:35:38 +0300

    finder

---
+++ sites/all/themes/tvkenya/scripts/tvkenya.js
@@ -0,0 +1,8 @@
+(function($) {
+	$(document).ready(function() {
+
+		// Add placeholder for search
+		$('#finder-form-show-finder #edit-show').attr('placeholder', 'Search channel/TV Show');
+	    
+	});
+})(jQuery);
